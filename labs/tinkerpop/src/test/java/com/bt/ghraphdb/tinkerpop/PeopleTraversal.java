2c7a771bfb5363dd1f07a26b0356004456f1479f
Arunkumar Krishnamoorthy <arunram.krish@gmail.com>
2016-03-01 07:37:03 +0530

    updated with more orientdb slides and updated code base

--- labs/tinkerpop/src/test/java/com/bt/ghraphdb/tinkerpop/PeopleTraversal.java
+++ labs/tinkerpop/src/test/java/com/bt/ghraphdb/tinkerpop/PeopleTraversal.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.tinkerpop.gremlin.process.traversal.P;
+import org.apache.tinkerpop.gremlin.process.traversal.Traverser;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.util.TraversalHelper;
@@ -19,7 +21,7 @@ import org.junit.Test;
 
 public class PeopleTraversal {
 	static Graph graph;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		Configuration configuration = new PropertiesConfiguration();
@@ -44,18 +46,36 @@ public class PeopleTraversal {
 
 	@Test
 	public void test() {
+		System.out.println(graph.vertices(0).next().property("name"));
+		
 		GraphTraversalSource travSource = graph.traversal(GraphTraversalSource.standard());
-		
-		GraphTraversal<Vertex, Vertex> traversal = travSource.V().has("name", "Person 0").out("Friend");
-		traversal.sideEffect(it -> System.out.println("Inside side effect " + it));
-		
+
+		GraphTraversal<Vertex, Vertex> traversal = travSource.V().has("name", P.eq("Person 0")).out("Friend");
+		traversal.sideEffect(it -> 
+			System.out.println("Inside side effect 1 " + ((Vertex) it.get()).property("name")));
+		traversal.out("Friend")
+				.sideEffect(it -> System.out.println("Inside side effect 2" + ((Vertex) it.get()).property("name")));
+
 		while (traversal.hasNext()) {
 			System.out.println("Printing");
 			Vertex v = traversal.next();
 			Object current = v.property("name").value();
+			// v.propert
 			System.out.println(current);
-			
-//			v.property("name", "Modified" + current);
-		}	
-	}
+
+			// v.property("name", "Modified" + current);
+		}
+
+		GraphTraversal<Vertex, Vertex> traversal2 = travSource.V();
+		traversal2.sideEffect(it -> 
+			System.out.println("Inside side effect 1 " + ((Vertex) it.get()).property("name")));
+		GraphTraversal<Vertex, Long> trav3 = traversal2.count();
+
+		while (trav3.hasNext()) {
+			System.out.println("Printing count");
+			System.out.println(trav3.next());
+
+			// v.property("name", "Modified" + current);
+		}
+}
 }
